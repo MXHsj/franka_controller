ab6cddc6ae0065d914b752f2d9b2e08a5237a99d
Ma Xihan <31639301+MXHsj@users.noreply.github.com>
2021-11-19 18:11:59 -0500

    update move to start

--- scripts/move_to_start.py
+++ scripts/move_to_start.py
@@ -5,22 +5,36 @@ import rospy
 from moveit_commander import MoveGroupCommander
 from actionlib_msgs.msg import GoalStatusArray
 
+home_config = 2
+
 if __name__ == '__main__':
   rospy.init_node('move_to_start')
   rospy.wait_for_message('move_group/status', GoalStatusArray)
   commander = MoveGroupCommander('panda_arm')
-  # franka default home pose
-  # commander.set_named_target('ready')
-  joint_goal = commander.get_current_joint_values()
 
-  # bedside US configuration
-  joint_goal[0] = 0.0
-  joint_goal[1] = -math.pi/6
-  joint_goal[2] = 0.0
-  joint_goal[3] = -2*math.pi/3
-  joint_goal[4] = 0.0
-  joint_goal[5] = math.pi/2
-  joint_goal[6] = -math.pi/3
+  joint_goal = commander.get_current_joint_values()
+  if home_config == 0:
+    # franka default home pose
+    commander.set_named_target('ready')
+    exit()
+  elif home_config == 1:
+    # front scan OCT configuration
+    joint_goal[0] = 0.0
+    joint_goal[1] = -math.pi/6
+    joint_goal[2] = 0.0
+    joint_goal[3] = -2*math.pi/3
+    joint_goal[4] = 0.0
+    joint_goal[5] = math.pi/2
+    joint_goal[6] = -math.pi/6
+  elif home_config == 2:
+    # bedside US configuration
+    joint_goal[0] = 0.0
+    joint_goal[1] = -math.pi/6
+    joint_goal[2] = 0.0
+    joint_goal[3] = -2*math.pi/3
+    joint_goal[4] = 0.0
+    joint_goal[5] = math.pi/2
+    joint_goal[6] = -math.pi/3
 
   commander.go(joint_goal, wait=True)
   commander.stop()
